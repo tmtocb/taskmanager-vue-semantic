f41130c68dd31fde7c2f36e59dc648ffec8d4353
tmtocb <kittz.esport@gmail.com>
2021-03-04 12:44:23 +0100

    add deleteTask function, add delete icon to Task grid

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var app = new Vue({
                     </div>
                   </div>
                   <div class="right floated three wide column">
+                    <i class="icon trash red" alt="Delete" v-on:click="app.deleteTask($event, task.id)"></i>
                   </div>
                 </div>
               </div>`
@@ -38,11 +39,20 @@ var app = new Vue({
   },
   methods: {
     toggleDone: function(event, id) {
+      event.stopImmediatePropagation();
       let task = this.tasks.find( item => item.id == id );
       if (task) {
         task.completed = !task.completed;
           console.log('task toggled');
       }
+    },
+    deleteTask: function(event, id) {
+      event.stopImmediatePropagation();
+      let taskIndex = this.tasks.findIndex( item => item.id == id );
+      if (taskIndex > -1) {
+        this.$delete(this.tasks, taskIndex);
+      }
+      console.log('task deleted');
     }
   }
 })       
