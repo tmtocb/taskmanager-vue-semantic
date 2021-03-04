ea0ea4527700afee0ef1e51eccc503ca735d0665
tmtocb <kittz.esport@gmail.com>
2021-03-04 02:43:10 +0100

    add hardcoded dummy data, display on index

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
+    tasks: [
+      { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
+      { id: 2, name: 'Todo 2', description: 'This is a todo', completed: false },
+      { id: 3, name: 'Todo 411', description: 'This is a todo', completed: true },
+      { id: 4, name: 'Todo 4', description: 'This is a todo', completed: true }
+    ],
     message: 'Hello World!!!!!!!!!'
   }         
 })       
